e4a065217cc258e501d7980da654be48169c42bf
Bugra Isguzar <biisguzar@gmail.com>
2020-04-25 22:05:57 +0300

    initial commit

---
+++ models.py
@@ -0,0 +1,32 @@
+import json
+import time
+import os
+from peewee import *
+
+database = SqliteDatabase("sqlite.db", field_types={'json': 'text'})
+
+class JSONField(Field):
+    field_type = 'json'
+
+    def db_value(self, value):
+        return json.dumps(value)
+
+    def python_value(self, value):
+        return json.loads(value)
+
+
+class BaseModel(Model):
+    class Meta:
+        database = database
+
+
+class Instagram(BaseModel):
+    username = CharField()
+    timestamp = IntegerField(default=lambda: int(time.time()))
+    data = JSONField()
+
+
+if not os.path.exists("./sqlite.db"):
+    print("Initializind db...")
+    with database:
+        database.create_tables([Instagram])
